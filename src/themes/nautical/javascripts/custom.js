7dcbe90bb6e86a6b7e3aba8176e6625a8f548401
elrushto <119621249+elrushto@users.noreply.github.com>
2023-11-07 22:25:24 -0500

    Updated custom.js

--- src/themes/nautical/javascripts/custom.js
+++ src/themes/nautical/javascripts/custom.js
@@ -1,27 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const elements = document.querySelectorAll('.nav-group-title');
   
   elements.forEach((el) => {
     const content = el.textContent.trim();
-
     if (['Queries', 'Mutations', 'Types', 'Definitions'].includes(content)) {
-      // Append the plus sign for expandable items
-//      appendExpandSign(el);
-
-//      const nextUl = el.nextElementSibling;
-//      if (nextUl && nextUl.tagName === 'UL') {
+      const nextUl = el.nextElementSibling;
+      if (nextUl && nextUl.tagName === 'UL') {
         // Hide the UL initially
-//        nextUl.style.display = 'none';
+        nextUl.style.display = 'none';
+        nextUl.classList.add('schema-section');
 
         // Assign an onclick function to toggle the UL
- //       el.addEventListener('click', () => toggleNavItems(nextUl, el));
+        el.addEventListener('click', () => toggleNavItems(nextUl));
+
+        // Monitor changes to the active class on the a elements
+        const links = nextUl.querySelectorAll('a');
+        links.forEach(link => {
+          new MutationObserver((mutationsList, observer) => {
+            for (let mutation of mutationsList) {
+              if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                // If the nav-scroll-active class was added, show the UL
+                if (link.classList.contains('nav-scroll-active')) {
+                  nextUl.style.display = '';
+                }
+              }
+            }
+          }).observe(link, { attributes: true });
+        });
 
         // Add specific class based on content
         addNavItemClass(el, content);
-      
+      }
     }
   });
 });
+
+function toggleNavItems(ulElement) {
+  const isHidden = ulElement.style.display === 'none';
+  ulElement.style.display = isHidden ? '' : 'none';
+}
 
 function addNavItemClass(navGroupTitle, content) {
   const classMap = {
@@ -30,7 +47,6 @@ function addNavItemClass(navGroupTitle, content) {
     'Types': 'types-nav-item',
   };
 
-  // Add the new class
   const className = classMap[content];
   if (className) {
     navGroupTitle.classList.add(className);
